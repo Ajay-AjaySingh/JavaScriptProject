ca40180be9a7bd11d1b050de06092777f30a3d94
Ajay <ajaysinghmbj@gmail.com>
2024-10-02 08:21:26 +0530

    separated products file different folder

--- Flipkart Project/index.js
+++ Flipkart Project/index.js
@@ -1,17 +1,3 @@
-const products = [
-    {
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqaUx796j3oN9x9DvyJBsy85Mf1-GICToxTA&s',
-        title: 'Sample Product 1',
-        price: '$22.45',
-        id: 'pro1'
-    },
-    {
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqaUx796j3oN9x9DvyJBsy85Mf1-GICToxTA&s',
-        title: 'Sample Product 2',
-        price: '$30.99',
-        id: 'pro2'
-    }
-];
 
 // Initialize an empty cart
 const cart = [];
